8642b7dbd04a9857c9f771468d75af2e2ddaf426
yashitz07 <yashitz07@gmail.com>
2025-05-16 20:01:09 +0530

    cors added

--- server/app.js
+++ server/app.js
@@ -12,6 +12,16 @@ app.use("/api/users", userRoutes);
 app.use("/api/loans", loanRoutes);
 app.get("/", (req, res) => res.send(""));
 
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://your-frontend-domain.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 mongoose
   .connect(process.env.MONGODB_SERVER_PORT, {
     useNewUrlParser: true,
